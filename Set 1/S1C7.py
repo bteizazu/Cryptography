56355460263091fdc77ff72f4be60649ebddab5f
bteizazu <teiza002@umn.edu>
2022-11-05 19:04:14 -0500

    Create S1C7.py

---
+++ Set 1/S1C7.py
@@ -0,0 +1,4 @@
+#Set 1, Challenge 7: AES in ECB mode
+
+## In terminal:
+## openssl enc -aes-128-ecb -pbkdf2 -d -a -K 59454c4c4f57205355424d4152494e45 -in /Users/bteizazu/Downloads/cryptochallenge37.txt -out 37decrypt.txt
